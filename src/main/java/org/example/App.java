cb1106cea865b31c907645aaf55ca7daafd68116
aleksejmoksancev <mokshantsev.aleksei>
2022-09-08 22:34:41 +0500

    some changes in master

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        //in recursion branch
+        //some changes in master
         System.out.println( "Hello World!" );
     }
 }
