1390972bc6ac885844ef971ac204c72a6cbef70b
aleksejmoksancev <mokshantsev.aleksei>
2022-09-08 22:35:34 +0500

    some changes in rec

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         //in recursion branch
+
+        //some changes in recrussion
         System.out.println( "Hello World!" );
     }
 }
