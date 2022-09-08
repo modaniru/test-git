b43ca563314177b295494f409ecae565ad4b004e
aleksejmoksancev <mokshantsev.aleksei>
2022-09-08 22:44:45 +0500

    try resolve a merge conflict

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,9 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        //in recursion branch
-
-        //some changes in recrussion
         System.out.println( "Hello World!" );
     }
 }
